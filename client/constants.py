a614c4e1ce9fec1f6fba5033f6f87739cc71a767
Cameron Redovian <naivoder@gmail.com>
2026-06-03 02:08:23 -0400

    Add type annotations to client constants.py

--- client/constants.py
+++ client/constants.py
@@ -1,22 +1,26 @@
-TARGET_WIDTH = 1280
-TARGET_HEIGHT = 960
+"""UI layout and rendering constants for the doodle client."""
 
-UI_FONT_SIZE = 14
-UI_PADDING = 10
-UI_CORNER_RADIUS = 12
-COLOR_SWATCH_SIZE = 22
-COLOR_PICKER_COLUMNS = 6
-ICON_SIZE = 32
-THICKNESS_OPTIONS = [2, 4, 6, 10, 16, 24]
-STROKE_HIT_TOLERANCE = 8
+from typing import Final
 
-GLASS_BG = (255, 255, 255, 40)
-GLASS_BORDER = (255, 255, 255, 80)
-GLASS_TEXT = (255, 255, 255)
-GLASS_HIGHLIGHT = (255, 255, 255, 60)
-TOOLTIP_BG = (30, 30, 30, 200)
+TARGET_WIDTH: Final[int] = 1280
+TARGET_HEIGHT: Final[int] = 960
 
-PICKER_COLORS = [
+UI_FONT_SIZE: Final[int] = 14
+UI_PADDING: Final[int] = 10
+UI_CORNER_RADIUS: Final[int] = 12
+COLOR_SWATCH_SIZE: Final[int] = 22
+COLOR_PICKER_COLUMNS: Final[int] = 6
+ICON_SIZE: Final[int] = 32
+THICKNESS_OPTIONS: Final[list[int]] = [2, 4, 6, 10, 16, 24]
+STROKE_HIT_TOLERANCE: Final[int] = 8
+
+GLASS_BG: Final[tuple[int, int, int, int]] = (255, 255, 255, 40)
+GLASS_BORDER: Final[tuple[int, int, int, int]] = (255, 255, 255, 80)
+GLASS_TEXT: Final[tuple[int, int, int]] = (255, 255, 255)
+GLASS_HIGHLIGHT: Final[tuple[int, int, int, int]] = (255, 255, 255, 60)
+TOOLTIP_BG: Final[tuple[int, int, int, int]] = (30, 30, 30, 200)
+
+PICKER_COLORS: Final[list[list[int]]] = [
     [231, 76, 60],   [192, 57, 43],   [211, 84, 0],
     [243, 156, 18],  [241, 196, 15],  [39, 174, 96],
     [46, 134, 193],  [41, 128, 185],  [142, 68, 173],
